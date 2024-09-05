0cc3703c9bfb9c98a7925bed7b0a8461ae8c4d73
Rudy <abcdupuisrudy@gmail.com>
2024-09-05 09:36:43 +0200

    initialisation du projet

---
+++ back/src/index.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const { Sequelize } = require("sequelize");
+
+const app = express();
+const port = 3000;
+
+const sequelize = new Sequelize("express_dev_db", "postgres", "postgres", {
+  host: "db",
+  dialect: "postgres",
+});
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, async () => {
+  try {
+    await sequelize.authenticate();
+    console.log(
+      "Connection to the database has been established successfully."
+    );
+  } catch (error) {
+    console.error("Unable to connect to the database:", error);
+  }
+  console.log(`App listening at http://localhost:${port}`);
+});
